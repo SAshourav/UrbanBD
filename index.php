07b9415beb23b0610c45942abda46cf2ddd1cc54
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2022-12-04 23:27:43 +0600

    getting product from database

--- index.php
+++ index.php
@@ -132,8 +132,13 @@
         <p>Here you can check out our featured products</p>
       </div>
       <div class="row mx-auto container-fluid">
+
+        <?php include('server/get_fetured_products.php'); ?>
+
+        <?php while($row= $featured_products->fetch_assoc()){ ?>
+
         <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-          <img class="img-fluid mb-3" src="assets/imgs/Featured1.jpg"/>
+          <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image'] ?>"/>
           <div class="star">
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
@@ -141,49 +146,13 @@
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
           </div>
-          <h5 class="p-name">Sports Shoes</h5>
-          <h4 class="p-price">$199.8</h4>
+          <h5 class="p-name"><?php echo $row['product_name']; ?></h5>
+          <h4 class="p-price">$<?php echo $row['product_price']; ?></h4>
           <button class="buy-btn">Buy Now</button>
         </div>
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-          <img class="img-fluid mb-3" src="assets/imgs/Featured2.jpg"/>
-          <div class="star">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-          <h5 class="p-name">School Bags</h5>
-          <h4 class="p-price">$199.8</h4>
-          <button class="buy-btn">Buy Now</button>
-        </div>
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-          <img class="img-fluid mb-3" src="assets/imgs/Featured3.jpg"/>
-          <div class="star">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-          <h5 class="p-name">School Bags</h5>
-          <h4 class="p-price">$199.8</h4>
-          <button class="buy-btn">Buy Now</button>
-        </div>
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-          <img class="img-fluid mb-3" src="assets/imgs/Featured4.jpg"/>
-          <div class="star">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-          <h5 class="p-name">School Bags</h5>
-          <h4 class="p-price">$199.8</h4>
-          <button class="buy-btn">Buy Now</button>
-        </div>
+        
+        <?php } ?>
+
       </div>
     </section>
 
