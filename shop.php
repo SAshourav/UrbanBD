a38a3c923d4693cfa1b7559687c15568c41a8fa1
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2022-12-19 16:33:14 +0600

    shop design fix

--- shop.php
+++ shop.php
@@ -101,7 +101,8 @@ $products = $stmt->get_result();  //array
 
         <!--Shop-->
 
-        <!-- Search Section-->
+    <div class="grid-container">
+           <!-- Search Section-->
         <section id="search" class="my-5 py-5 ms-2">
           <div class="container mt-5 py-5 ms-2">
             <p>Search Product</p>
@@ -163,45 +164,47 @@ $products = $stmt->get_result();  //array
         </section>
 
         <!-- Featured Section-->
-    <section id="featured" class="my-5 py-5">
-        <div class="container mt-5 py-5">
-          <h3>Our Products</h3>
-          <hr>
-          <p>Here you can check out our products</p>
-        </div>
-        <div class="row mx-auto container">
-
-      <?php while ($row = $products->fetch_assoc()) { ?>
-
-          <div onclick="window.location.href='single_product.html';" class="product text-center col-lg-3 col-md-4 col-sm-12">
-            <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>"/>
-            <div class="star">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
+        <section id="featured" class="my-5 py-5">
+            <div class="container mt-5 py-5">
+              <h3>Our Products</h3>
+              <hr>
+              <p>Here you can check out our products</p>
             </div>
-            <h5 class="p-name"><?php echo $row['product_name']; ?></h5>
-            <h4 class="p-price"><?php echo $row['product_price']; ?></h4>
-            <a class="btn shop-buy-btn" href="<?php echo "single_product.php?product_id =" .$row['product_id'];?>">Buy Now</a>
-          </div>
-          
-          <?php } ?>
-          
-          <nav aria-label="page navigation example">
-            <ul class="pagination mt-5">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-          </nav>
+            <div class="row mx-auto container">
 
-        </div>
-    </section>
+          <?php while ($row = $products->fetch_assoc()) { ?>
 
+              <div onclick="window.location.href='single_product.html';" class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>"/>
+                <div class="star">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </div>
+                <h5 class="p-name"><?php echo $row['product_name']; ?></h5>
+                <h4 class="p-price"><?php echo $row['product_price']; ?></h4>
+                <a class="btn shop-buy-btn" href="<?php echo "single_product.php?product_id =" .$row['product_id'];?>">Buy Now</a>
+              </div>
+              
+              <?php } ?>
+              
+              <nav aria-label="page navigation example">
+                <ul class="pagination mt-5">
+                  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+              </nav>
+
+            </div>
+        </section>
+
+    </div>
+       
 
 
 
