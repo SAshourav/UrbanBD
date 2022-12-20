a6bb5bcda8eea763b288e1c2549ee79e13f1bc18
Sabbir Ahmed <sabbirahmed2013303@gmail.com>
2022-12-20 15:59:15 +0600

    organizing the code using layout section

---
+++ contact.php
@@ -0,0 +1,24 @@
+  <?php include('layout/header.php'); ?>
+   
+    <!--Contact-->
+
+    <section id="contact" class="container my-5 py-5">
+        <div class="container text-center mt-5">
+            <h3>Contact Us</h3>
+            <hr class="mx-auto">
+            <p class="w-50 mx-auto">
+                Phone Number: <span> 123 456 789</span>
+            </p>
+            <p class="w-50 mx-auto">
+                Email Address: <span> kazi.mahmud7@northsouth.edu</span>
+            </p>
+            <p class="w-50 mx-auto">
+                We work 24/7/365 to answer your questions
+            </p>
+        </div>
+    </section>
+
+   
+     <!--Footer-->
+
+     <?php include('layout/footer.php'); ?>
---
+++ layout/footer.php
@@ -0,0 +1,77 @@
+<!--Footer-->
+
+<footer class="mt-5 py-5">
+       <div class="row container mx-auto pt-5">
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <img class="logo" src="assets/imgs/logo.png"/>
+          <p class="pt-3">We provide the best products for the most affordable prices</p>
+        </div>
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Featured</h5>
+          <ul class="text-uppercase">
+            <li><a href="#">men</a></li>
+            <li><a href="#">women</a></li>
+            <li><a href="#">boys</a></li>
+            <li><a href="#">girls</a></li>
+            <li><a href="#">new arrivals</a></li>
+            <li><a href="#">clothes</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Contact Us</h5>
+          <div>
+            <h6 class="text-uppercase">Address</h6>
+            <p>Basundhara R/A , Dhaka</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Phone</h6>
+            <p>123 456 789</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Email</h6>
+            <p>sabbir.ahmed5@northsouth.edu</p>
+            <p>sajid.mahmud7@northsouth.edu</p>
+          </div>
+        </div>
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Instagram</h5>
+          <div class="row">
+            <img src="assets/imgs/instagram/bag.jpg" class="img-fluid w-25 h-100 m-2"/>
+            <img src="assets/imgs/instagram/jacket.jpg" class="img-fluid w-25 h-100 m-2"/>
+            <img src="assets/imgs/instagram/shoe.jpg" class="img-fluid w-25 h-100 m-2"/>
+            <img src="assets/imgs/Featured1.jpg" class="img-fluid w-25 h-100 m-2"/>
+            <img src="assets/imgs/Featured2.jpg" class="img-fluid w-25 h-100 m-2"/>
+          </div>
+        </div>
+       </div>
+
+
+        <div class="copyright mt-5">
+          <div class="row container mx-auto">
+            <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+             <img src="assets/imgs/payment.jpg"/> 
+            </div>
+            <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
+               <p>eCommerce @ 2030 All Right Reserved</p>
+             </div>
+             <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+              <a href="#"><i class="fab fa-facebook"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-snapchat"></i></a>
+             </div>
+          </div>
+        </div>
+
+     </footer>
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
